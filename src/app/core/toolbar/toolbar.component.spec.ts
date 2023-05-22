da1dfc17b43e81cee019cb110e145fc6006db42b
matt-primrose <matt.c.primrose@intel.com>
2023-05-22 10:21:39 -0700

    build: setup app layout

---
+++ src/app/core/toolbar/toolbar.component.spec.ts
@@ -0,0 +1,22 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ToolbarComponent } from './toolbar.component';
+import { MatToolbarModule } from '@angular/material/toolbar'
+
+describe('ToolbarComponent', () => {
+  let component: ToolbarComponent;
+  let fixture: ComponentFixture<ToolbarComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [ToolbarComponent],
+      imports: [MatToolbarModule]
+    });
+    fixture = TestBed.createComponent(ToolbarComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
